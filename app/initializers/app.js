bf5c6fb1fed363b0c65627cecd176461492c06c6
Rico Sta. Cruz <hi@ricostacruz.com>
2013-05-31 07:58:58 +0800

    Omg, twitter auth.

--- app/initializers/app.js
+++ app/initializers/app.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var passport = require('passport');
 
 module.exports = function(app) {
+  var token = app.conf('secret_token') || '...........';
+
   app.set('view engine', 'jade');
 
   app.configure('development', function() {
@@ -8,13 +11,16 @@ module.exports = function(app) {
     app.use(express.logger('dev'));
   });
 
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
   app.use(express['static'](app.path('public')));
+  app.use(express.cookieParser(token));
+  app.use(express.bodyParser());
+  app.use(express.session({ key: 'session', secret: token }));
+  app.use(express.methodOverride());
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
 
   app.configure('development', function() {
     app.use(express.errorHandler());
   });
-
 };
--- app/routes/home.js
+++ app/routes/home.js
@@ -1,4 +1,8 @@
 var express = require('express');
+
+var passport = require('passport');
+var TwitterStrategy = require('passport-twitter').Strategy;
+
 module.exports = function(app) {
   var admin = require('./admin_filters');
 
@@ -12,40 +16,72 @@ module.exports = function(app) {
     redirect('/admin/accounts'));
 
   app.get('/admin/accounts',
-    setAccounts,
+    getAccounts,
     index);
 
-  app.get('/admin/accounts/new',
-    new_);
+  // New form
+  app.get('/admin/accounts/new/twitter',
+    newTwitter);
 
+  // Get-edit
   app.get('/admin/accounts/:id',
-    setAccount,
+    getAccount,
     show);
 
+  // Create
   app.post('/admin/accounts',
     create);
 
   app.post('/admin/accounts/:id',
-    setAccount,
+    getAccount,
     update);
 
   app.del('/admin/accounts/:id',
-    setAccount,
+    getAccount,
     destroy);
 
+  app.get('/admin/accounts/:id/auth/twitter',
+    getAccount,
+    ensureAccountIs('twitter'),
+    passportTwitter);
+
+  var Account = require('../../lib/models/account');
+
   function index(req, res) {
-    res.render('accounts-index');
+    res.render('accounts/index');
   }
 
-  function new_(req, res) {
-    res.render('accounts-new');
+  function newTwitter(req, res) {
+    res.locals.account = Account.build({
+      service: 'twitter',
+      name: 'twitter'
+    });
+    res.render('accounts/new-twitter');
   }
 
   function show(req, res) {
-    res.send('');
+    var account = res.locals.account;
+    res.render('accounts/show-' + account.service);
   }
 
+  /*
+   * @input body.account
+   * @output render
+   */
   function create(req, res) {
+    var data = req.body.account;
+
+    var account = Account.build(data);
+    account.setCredentials(data.credentials);
+    res.locals.account = account;
+
+    var service = account.service;
+
+    account.save().success(function() {
+      res.redirect('/admin/accounts/' + account.id);
+    }).error(function() {
+      res.render('accounts/new-'+service);
+    });
   }
 
   function update(req, res) {
@@ -54,14 +90,70 @@ module.exports = function(app) {
   function destroy(req, res) {
   }
 
-  // Retrieve an account.
-  function setAccount(req, res, next) {
-    next();
+  /*
+   * Retrieves an account for editing.
+   */
+  function getAccount(req, res, next) {
+    var id = req.params.id;
+
+    wrap(Account.find(id), next, function(account) {
+      res.locals.account = account;
+    });
   }
 
-  function setAccounts(req, res, next) {
-    res.locals.accounts = ['tom', 'harry'];
-    next();
+  function ensureAccountIs(service) {
+    return function(req, res, next) {
+      var account = res.locals.account;
+      if (account.service !== service)
+        return next(new Error("Wrong service (" + account.service + " is not " + service + ")"));
+      next();
+    };
+  }
+
+  function passportTwitter(req, res, next) {
+    var account = res.locals.account;
+    var id = res.locals.account.id;
+
+    passport.use(new TwitterStrategy({
+      consumerKey: account.getCredentials().consumerKey,
+      consumerSecret: account.getCredentials().consumerSecret,
+      callbackURL: "http://127.0.0.1:4567/admin/accounts/"+id+"/auth/twitter/callback"
+    },
+    function(token, tokenSecret, profile, done) {
+      // Save credentials
+      account.extendCredentials({
+        accessToken: token,
+        accessTokenSecret: tokenSecret,
+        username: profile.username,
+        displayName: profile.displayName,
+        photo: profile.photos[0].value
+      });
+
+      account.save().success(function() {
+        done();
+      }).error(function(e) {
+        done(e);
+      });
+    }));
+
+    passport.authenticate('twitter').apply(this, arguments);
+  }
+
+  app.get('/admin/accounts/:id/auth/twitter/callback', function(req, res, next) {
+    var id = req.params.id;
+    passport.authenticate('twitter', {
+        successRedirect: '/admin/accounts/'+id,
+        failureRedirect: '/admin/accounts/'+id
+    }).apply(this, arguments);
+  });
+
+  /*
+   * Retrieves the list of accounts for indexing
+   */
+  function getAccounts(req, res, next) {
+    wrap(Account.findAll(), next, function(accounts) {
+      res.locals.accounts = accounts;
+    });
   }
 
   function nil(req, res) {
@@ -70,5 +162,20 @@ module.exports = function(app) {
 
   function redirect(url) {
     return function(req, res) { res.redirect(url); };
+  }
+
+  /*
+   * Wraps a Sequelize promise
+   */
+  function wrap(promise, next, callback) {
+    promise
+      .success(function(data) {
+        if (!data) return next(404);
+        if (callback) callback(data);
+        next();
+      })
+      .error(function(err) {
+        next(err);
+      });
   }
 };
