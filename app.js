a724b28a57409c3a2ce59093b2e0319d48eae47b
Rico Sta. Cruz <hi@ricostacruz.com>
2013-07-01 01:26:09 +0800

    Listen.

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ require('expo-connect_assets')(app);
 
 global.util = require('util');
 global.inspect = require('util').inspect;
+
+// Listen when invoked as `node .` or `node app.js`
+if (!module.parent) {
+  var port = process.env.PORT || 3000;
+  app.log.debug("Listening at port " + port);
+  app.load().listen(port);
+}
